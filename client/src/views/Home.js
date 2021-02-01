2c5f1b3775e334028c19ddd4d67589029e05eff0
Stamati Morellas <smorellas@protonmail.com>
2021-02-01 13:44:55 -0600

    Added initial push
    Experiment with using @geist-ui/react components for front-end

---
+++ client/src/views/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// TODO: IMPORT SECTIONS
+// Example:
+//  import Hero from '../components/sections/Hero';
+//  import FeaturesTiles from '../components/sections/FeaturesTiles';
+//  import FeaturesSplit from '../components/sections/FeaturesSplit';
+//  import Testimonial from '../components/sections/Testimonial';
+//  import Cta from '../components/sections/Cta';
+
+const Home = () => {
+
+  return (
+    <>
+      // TODO
+    </>
+  );
+}
+
+export default Home;
